f7f9bb38683b860022184ac0a94e80c7d48d6697
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-20 14:45:38 +0530

    Add postImage field

---
+++ server/models/Post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  postImage: {
+    type: String,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  datePosted: {
+    type: Date,
+    default: new Date(),
+  },
+  postedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
