402f045519e793122b4d5fb0f425f1bbd3a7210a
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-21 01:49:17 +0530

    Custom theme

---
+++ client/src/theme.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#010101",
+    },
+    secondary: {
+      main: "#ee1d52",
+    },
+  },
+});
