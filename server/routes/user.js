862ba2cc6910b534f4f6ac11216f72a0720d5c1b
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-21 20:16:02 +0530

    Route for user profile, other user profile

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,12 +3,43 @@ const router = express.Router();
 const verify = require("../middlewares/verify");
 const User = require("../models/User");
 const Post = require("../models/Post");
+
+/**
+ * @description User Profile
+ * @route /user/profile/view
+ */
+
+router.get("/profile", verify, async (req, res) => {
+  try {
+    const user = await User.findById(req.user);
+    const posts = await Post.find({ postedBy: req.user }).sort({
+      datePosted: 1,
+    });
+    res.json({
+      data: {
+        result: {
+          user,
+          posts,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      data: {
+        type: "error",
+        message: "Internal server error",
+      },
+    });
+  }
+});
+
 /**
  * @description View Other user profile, shows only latest 6 posts
  * @route /user/profile/:id
  */
 
-router.get("/profile/:id", verify, async (req, res) => {
+router.get("/profile/view/:id", verify, async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const posts = await Post.find({ postedBy: req.params.id })
