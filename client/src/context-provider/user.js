021c563321f2bb98ceb009df6e2700a984d3accf
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-21 20:09:29 +0530

    Add User context

---
+++ client/src/context-provider/user.js
@@ -0,0 +1,18 @@
+import React, { useReducer, createContext } from "react";
+import { userReducer } from "../reducer/userReducer";
+export const UserContext = createContext({});
+
+const UserProvider = ({ children }) => {
+  const [userState, dispatch] = useReducer(userReducer, {
+    profile: null,
+    posts: null,
+  });
+
+  return (
+    <UserContext.Provider value={{ dispatch, user: userState }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
