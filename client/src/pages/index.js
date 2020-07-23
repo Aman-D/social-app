909cbfcd27f4785bbb555891809f39a0c8609d18
Aman Dhurwey <2017030@iiitdmj.ac.in>
2020-07-23 19:07:15 +0530

    Settings page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as AuthPage } from "./auth";
 export { default as DashboardPage } from "./dashboard";
 export { default as LandingPage } from "./landing";
 export { default as HomePage } from "./home";
+export { default as SettingsPage } from "./settings";
---
+++ client/src/pages/settings.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { Grid, Paper, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { settingLinks } from "../helper/settingsLink";
+import { ArrowRight } from "@material-ui/icons";
+import { useHistory } from "react-router-dom";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: "100%",
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  tab: {
+    padding: theme.spacing(2),
+    marginTop: theme.spacing(1),
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));
+
+const Settings = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  return (
+    <div className={classes.root}>
+      <Grid item xs={12}>
+        <Paper className={classes.paper}>
+          <Typography variant="h2">Social App</Typography>
+          <Typography variant="subtitle2">
+            This app is made as a practice project and ensures no data will be
+            used for unfair means. Contact dhurweyrock@gmail.com for any
+            queries.
+          </Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        {settingLinks.map(({ title, path }, index) => (
+          <Paper
+            key={index}
+            className={classes.tab}
+            onClick={() => history.push(path, { from: "/settings" })}
+          >
+            <Typography className={classes.heading}>{title}</Typography>
+            <ArrowRight />
+          </Paper>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default Settings;
